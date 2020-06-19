2f4c218e107bc16e8d8ec3b8d7d0ea04f4196908
An-Shan <62011948+An-Shan@users.noreply.github.com>
2020-06-19 17:26:02 +0800

    update
    add whole folder prdiction

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ model.add(Dense(11, activation = 'softmax'))
 
 model.load_weights('0619_01_weights.h5')
 
+# 測試單一資料並印出該圖片
 from keras.preprocessing.image import  image
 import numpy as np
 import matplotlib.pyplot as plt
@@ -33,3 +34,30 @@ try:
 
 except Exception as e:
     print({'msg':str(e).split(':')[0]})
+    
+# 測試folder內所有資料    
+train_datagen = ImageDataGenerator(rescale = 1.0 / 255 )
+test_datagen = ImageDataGenerator(rescale = 1.0 / 255 )
+
+train_generator = train_datagen.flow_from_directory(
+        'data3/train',
+        target_size = (224, 224),
+        class_mode = 'categorical')
+test_generator = test_datagen.flow_from_directory('Test_data', target_size=(224, 224),batch_size=1,class_mode='categorical', shuffle=False,)
+
+test_generator.reset()
+pred = model.predict_generator(test_generator)
+
+predicted_class_indices = np.argmax(pred, axis=1)
+labels = (train_generator.class_indices)
+label = dict((v,k) for k,v in labels.items())
+
+# 建立test標籤與真實標籤的關係
+predictions = [label[i] for i in predicted_class_indices]
+
+#建立預測結果和文件名之間的關係
+filenames = test_generator.filenames
+for idx in range(len(filenames )):
+    print('predict  %s' % ((predictions[idx])))
+    print('title    %s' % filenames[idx])
+    print('')
